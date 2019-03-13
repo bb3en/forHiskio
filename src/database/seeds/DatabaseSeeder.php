e7753ddbfc59d931a0b153956845e3b356cf10b6
unknown <bb3en.yang@gmail.com>
2019-03-13 11:41:05 +0800

    init

---
+++ src/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+
+    }
+}
